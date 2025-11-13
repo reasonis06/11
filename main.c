4739d9eefa22b8eee7a7bd21beab2a874585593c
이성은 <katrina060208@gmail.com>
2025-11-13 15:44:51 +0900

    실습5

--- main.c
+++ main.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int i;
-	int grade[5];
-	int sum = 0;
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i] = ", i);
-		scanf("%d", &grade[i]);
-	}
+	*p = 200;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
 	
-		
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		sum = sum + *(grade+i);
-	}
-	
-	printf("Average : %i\n", sum/5);
-	
-  system("PAUSE");	
-  return 0;
+	**q = 300;
+	printf("i = %i, *p = %d, **q = %d\n", i, *p, **q);
+
+	system("PAUSE");
+	return 0;
 }
 
